d8794209a81f88ed2ea513b256c27a5d7f203993
naol-A <45383986+naol-A@users.noreply.github.com>
2018-12-04 01:09:34 -0800

    Add files via upload

---
+++ source_code/inverted indexer/index.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace inverted_indexer
+{
+    partial class index
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(index));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(444, 49);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(283, 268);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 92);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(380, 173);
+            this.listBox1.TabIndex = 12;
+            this.listBox1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(313, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Ready";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 20);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 63);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Hide detail";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 271);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(380, 20);
+            this.progressBar1.TabIndex = 13;
+            this.progressBar1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Choose directory";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(284, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(627, 323);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 19;
+            this.textBox2.Visible = false;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.SpringGreen;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(398, 136);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(17, 85);
+            this.button5.TabIndex = 20;
+            this.button5.Text = ">";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(498, 330);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Enter document number:";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(371, 333);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "About";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 355);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "index";
+            this.Text = "Inverted file indexer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ source_code/inverted indexer/index.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace inverted_indexer
+{
+    public partial class index : Form
+    {
+        FolderBrowserDialog fld = new FolderBrowserDialog();
+        ArrayList load = new ArrayList();
+        ArrayList terms = new ArrayList();
+        public index()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = @"C:\Users\HP\Desktop\index";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Visible == true)
+            {
+                listBox1.Hide();
+                progressBar1.Location = new Point(12, 92);
+                button4.Text = "Show detail";
+            }
+            else if(listBox1.Visible == false)
+            {
+                listBox1.Show();
+                progressBar1.Location = new Point(12, 271);
+                button4.Text = "Hide detail";
+            }
+        }
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fld.ShowDialog();
+            if (fld.SelectedPath != "")
+            {
+                textBox1.Text = fld.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            load.Clear();
+            if (Directory.Exists(textBox1.Text))
+            {
+                string[] files = Directory.GetFiles(textBox1.Text);
+                Directory.CreateDirectory(textBox1.Text + @"\index");
+                StreamWriter wrt = new StreamWriter(textBox1.Text + @"\index\vocabulary.txt", false);
+                StreamWriter wrt2 = new StreamWriter(textBox1.Text + @"\index\posting.txt", false);
+                listBox1.Items.Clear();
+                progressBar1.Value = 0;
+                foreach (string str in Directory.EnumerateFiles(textBox1.Text))
+                {
+                    i++;
+                    load.Add(File.ReadAllText(str));
+                    listBox1.Items.Add(i + " : " + str + " ... Done");
+                }
+                terms.Clear();
+                //tokenization
+                for (int lp = 0; lp < i; lp++)
+                {
+                    foreach (Match res in Regex.Matches(load[lp].ToString(), @"\w*\s"))
+                    {
+                        terms.Add(res.ToString());
+                        wrt2.Write(res + "\t" + files[lp]);
+                        wrt2.WriteLine();
+                    }
+                }
+                //sorting
+                terms.Sort();
+                //stopword removal
+                string tm = "";
+                for (int lp3 = 0; lp3 < terms.Count; lp3++)
+                {
+                    tm = terms[lp3].ToString();
+                    if (Regex.IsMatch(tm, @"be") || Regex.IsMatch(tm, @"it") ||
+                       Regex.IsMatch(tm, @"did") || Regex.IsMatch(tm, @"let") ||
+                       Regex.IsMatch(tm, @"has") || Regex.IsMatch(tm, @"me") ||
+                       Regex.IsMatch(tm, @"(i|I)") || Regex.IsMatch(tm, @"so") ||
+                       Regex.IsMatch(tm, @"the") || Regex.IsMatch(tm, @"told") ||
+                       Regex.IsMatch(tm, @"you") || Regex.IsMatch(tm, @"was") ||
+                       Regex.IsMatch(tm, @"with") || Regex.IsMatch(tm, @"are") ||
+                       Regex.IsMatch(tm, @"can") || Regex.IsMatch(tm, @"by") ||
+                       Regex.IsMatch(tm, @"and") || Regex.IsMatch(tm, @"a") || Regex.IsMatch(tm, @"\s"))
+                    {
+                        terms.RemoveAt(lp3);
+                    }
+                }
+                //stemming
+                string stm = "";
+                for (int lp4 = 0; lp4 < terms.Count; lp4++)
+                {
+                    stm = terms[lp4].ToString();
+                    if (Regex.IsMatch(terms[lp4].ToString(), @"\w*ous") ||
+                        Regex.IsMatch(terms[lp4].ToString(), @"\w*ing"))
+                    {
+                        stm = stm.Remove(stm.Length - 4);
+                        terms[lp4] = stm;
+                    }
+                    else if (Regex.IsMatch(terms[lp4].ToString(), @"\w*ed") ||
+                             Regex.IsMatch(terms[lp4].ToString(), @"\w*er"))
+                    {
+                        stm = stm.Remove(stm.Length - 3);
+                        terms[lp4] = stm;
+                    }
+                    else if (Regex.Matches(terms[lp4].ToString(), @"\w*n") != null)
+                    {
+                        stm = stm.Remove(stm.Length - 1);
+                        terms[lp4] = stm;
+                    }
+                }
+                string trm = "";
+                int tf = 0;
+                foreach (string str in terms)
+                {
+                    trm = str + "\n";
+                }
+                //compute tf
+                for (int lp2 = 0; lp2 < terms.Count; lp2++)
+                {
+                    trm = terms[lp2].ToString();
+                    foreach (Match res in Regex.Matches(trm, terms[lp2].ToString()))
+                    {
+                        tf++;
+                        terms.Remove(res.ToString());
+                    }
+                    terms.Insert(lp2, trm + "," + tf);
+                    wrt.Write(terms[lp2]);
+                    wrt.WriteLine();
+                    tf = 0;
+                }
+                //split
+                button2.Text = "Refresh";
+                button4.Show();
+                listBox1.Show();
+                progressBar1.Show();
+                progressBar1.Value = 100;
+                wrt.Close();
+                wrt2.Close();
+            }
+            else
+            {
+                MessageBox.Show("folder doesn't exist choose existing folder", "warning");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button2.Text = "Start";
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            int i = 0;
+            if (Int32.TryParse(textBox2.Text,out i))
+            {
+                if (i <= load.Count&& i>0)
+                {
+                    richTextBox1.Text = load[i-1].ToString();
+                }
+                else
+                {
+                    label2.Text = "index out of bound";
+                }
+            }
+            else
+            {
+                label2.Text = "invalid index value";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Visible == false)
+            {
+                richTextBox1.Show();
+                textBox2.Show();
+                label3.Show();
+                this.Size = new Size(779, 394);
+                button5.Location = new Point(734, 136);
+                button5.Text = "<";
+            }
+            else
+            {
+                richTextBox1.Hide();
+                textBox2.Hide();
+                label3.Hide();
+                this.Size = new Size(434, 394);
+                button5.Location = new Point(398, 136);
+                button5.Text = ">";
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Product name: inverted indexer\n" +
+                            "Version:v0.1\n" +
+                            "Copyright:2018 nanosoft all rigths reserved\n" +
+                            "Description:inverterd index file gen\n"+
+                            "group member:\n" +
+                            "\t1,naol areaga\n" +
+                            "\t2,tebibu solomon\n" +
+                            "\t3,simon mulugeta\n" +
+                            "\t4,yonathan hilezghi\n" +
+                            "\t5,lidya \n" +
+                            "\t6,yeabsira getachew\n" +
+                            "\t7,nahom nayzgi\n" +
+                            "\t8,omer mohamed\n");
+        }
+    }
+}
